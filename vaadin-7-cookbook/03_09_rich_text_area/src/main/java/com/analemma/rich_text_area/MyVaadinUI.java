e253accf91229afa08717e9e9f77d411e1fbd0b2
Mladen Čikara <mladen.cikara@gmail.com>
2014-02-09 12:45:13 +0100

    Dodao poglavlje 03_09. Pokazuje kako se pomoću CSS-a mogu sakriti i izmjeniti elementi postojećeg sučelja

---
+++ vaadin-7-cookbook/03_09_rich_text_area/src/main/java/com/analemma/rich_text_area/MyVaadinUI.java
@@ -0,0 +1,29 @@
+
+package com.analemma.rich_text_area;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.RichTextArea;
+import com.vaadin.ui.UI;
+
+@Theme("mytheme")
+@SuppressWarnings("serial")
+public class MyVaadinUI extends UI {
+
+	@WebServlet(value = "/*", asyncSupported = true)
+	@VaadinServletConfiguration(productionMode = false, ui = MyVaadinUI.class, widgetset = "com.analemma.rich_text_area.AppWidgetSet")
+	public static class Servlet extends VaadinServlet {
+	}
+
+	@Override
+	protected void init(VaadinRequest request) {
+
+		RichTextArea richTextArea = new RichTextArea();
+		richTextArea.setStyleName("my");
+		setContent(richTextArea);
+	}
+}
