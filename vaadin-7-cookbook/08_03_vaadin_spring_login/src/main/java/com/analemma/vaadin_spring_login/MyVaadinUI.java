bdac990c8da893cf6b77aa0f41409fa6d6eb5b50
Mladen Čikara <mladen.cikara@gmail.com>
2014-03-16 09:15:14 +0100

    postavio Atmosphare BroadcasterCacheClass

--- vaadin-7-cookbook/08_03_vaadin_spring_login/src/main/java/com/analemma/vaadin_spring_login/MyVaadinUI.java
+++ vaadin-7-cookbook/08_03_vaadin_spring_login/src/main/java/com/analemma/vaadin_spring_login/MyVaadinUI.java
@@ -1,6 +1,7 @@
 package com.analemma.vaadin_spring_login;
 
 import javax.servlet.ServletContext;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpSession;
 
@@ -24,20 +25,22 @@ import com.vaadin.ui.UI;
 @PreserveOnRefresh
 public class MyVaadinUI extends UI {
 
-  @WebServlet(value = "/*", asyncSupported = true)
+  @WebServlet(value = "/*", asyncSupported = true, initParams = {@WebInitParam(
+      name = "org.atmosphere.cpr.broadcasterCacheClass",
+      value = "org.atmosphere.cache.UUIDBroadcasterCache")})
   @VaadinServletConfiguration(productionMode = false, ui = MyVaadinUI.class,
       widgetset = "com.analemma.vaadin_spring_login.AppWidgetSet")
   public static class Servlet extends VaadinServlet {
   }
 
   private ApplicationContext applicationContext;
-  
+
   private static final Logger logger = LoggerFactory.getLogger(MyVaadinUI.class);
 
   @Override
   protected void init(VaadinRequest request) {
     logger.info("Init started");
-    
+
     WrappedSession session = request.getWrappedSession();
     HttpSession httpSession = ((WrappedHttpSession) session).getHttpSession();
     ServletContext servletContext = httpSession.getServletContext();
@@ -52,6 +55,8 @@ public class MyVaadinUI extends UI {
 
     navigator.navigateTo("login");
     setNavigator(navigator);
+    
+    logger.info("Init finished");
   }
 
   public ApplicationContext getApplicationContext() {
