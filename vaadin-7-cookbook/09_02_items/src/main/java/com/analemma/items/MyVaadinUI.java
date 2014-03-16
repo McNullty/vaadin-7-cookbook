b6118fd950c55e595fb15e210598b654c4e6dc28
Mladen Čikara <mladen.cikara@gmail.com>
2014-03-16 22:28:35 +0100

    Dodao poglavlje 09_02 Items

---
+++ vaadin-7-cookbook/09_02_items/src/main/java/com/analemma/items/MyVaadinUI.java
@@ -0,0 +1,24 @@
+package com.analemma.items;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.UI;
+
+@Theme("mytheme")
+@SuppressWarnings("serial")
+public class MyVaadinUI extends UI {
+
+    @WebServlet(value = "/*", asyncSupported = true)
+    @VaadinServletConfiguration(productionMode = false, ui = MyVaadinUI.class, widgetset = "com.analemma.items.AppWidgetSet")
+    public static class Servlet extends VaadinServlet {
+    }
+
+    @Override
+    protected void init(VaadinRequest request) {
+        setContent(new AdminPage());
+    }
+}
