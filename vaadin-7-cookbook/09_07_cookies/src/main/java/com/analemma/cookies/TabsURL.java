4dfb052887dbaf3a6c296f878628261613f2dfab
Mladen Čikara <mladen.cikara@gmail.com>
2014-03-17 22:57:43 +0100

    Dodao template svih projekata

---
+++ vaadin-7-cookbook/09_07_cookies/src/main/java/com/analemma/cookies/TabsURL.java
@@ -0,0 +1,84 @@
+package com.analemma.cookies;
+
+import java.util.Iterator;
+
+import javax.servlet.http.Cookie;
+
+import com.vaadin.server.VaadinService;
+import com.vaadin.server.VaadinServletRequest;
+import com.vaadin.server.VaadinServletResponse;
+import com.vaadin.shared.ui.label.ContentMode;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TabSheet;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+@SuppressWarnings("serial")
+public class TabsURL extends TabSheet {
+  private static final String tabNames[] =
+      {"Home", "Contractors", "Customers", "Employees", "Help"};
+  private static final String LAST_TAB_COOKIE_NAME = "vaadin_last_tab_name";
+  private static final String PATH = "/data";
+
+  public TabsURL() {
+    createTabs(tabNames);
+    addSelectedTabChangeListener(new SelectedTabChangeListener() {
+      @Override
+      public void selectedTabChange(final SelectedTabChangeEvent event) {
+        final String selectedTabName = event.getTabSheet().getSelectedTab().getCaption();
+        UI.getCurrent().getPage().setUriFragment(selectedTabName);
+      }
+    });
+  }
+
+  private void createTabs(final String tabNames[]) {
+    for (final String tabName : tabNames) {
+      final VerticalLayout layout = new VerticalLayout();
+      layout.setCaption(tabName);
+      layout.addComponent(new Label("<h1>" + tabName + "</h1>", ContentMode.HTML));
+      layout.setHeight(400, Unit.PIXELS);
+      addComponent(layout);
+    }
+  }
+
+  public void selectTab() {
+    String fragment = UI.getCurrent().getPage().getUriFragment();
+    if (fragment == null) {
+      fragment = getLastTabNameFromCookies();
+    }
+    final Iterator<Component> iterator = this.iterator();
+    while (iterator.hasNext()) {
+      final Component tab = iterator.next();
+      final String name = tab.getCaption().toLowerCase();
+      if (fragment.toLowerCase().equals(name)) {
+        setSelectedTab(tab);
+        setLastTabNameToCookies(name);
+        return;
+      }
+    }
+    setSelectedTab(0);
+  }
+
+
+  private void setLastTabNameToCookies(final String tabName) {
+    final Cookie lastTabCookie = new Cookie(LAST_TAB_COOKIE_NAME, tabName);
+    lastTabCookie.setPath(PATH);
+    final VaadinServletResponse response =
+        (VaadinServletResponse) VaadinService.getCurrentResponse();
+    response.addCookie(lastTabCookie);
+  }
+
+  private String getLastTabNameFromCookies() {
+    final Cookie[] cookies =
+        ((VaadinServletRequest) VaadinService.getCurrentRequest()).getCookies();
+    for (final Cookie cookie : cookies) {
+      if (LAST_TAB_COOKIE_NAME.equals(cookie.getName())) {
+        return cookie.getValue();
+      }
+    }
+    return "";
+  }
+
+
+}
---
+++ vaadin-7-cookbook/11_07_vaadin_production_mode/src/main/java/com/analemma/vaadin_production_mode/MyVaadinUI.java
@@ -0,0 +1,23 @@
+package com.analemma.vaadin_production_mode;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.UI;
+
+@Theme("mytheme")
+@SuppressWarnings("serial")
+public class MyVaadinUI extends UI {
+
+    @WebServlet(value = "/*", asyncSupported = true)
+    @VaadinServletConfiguration(productionMode = false, ui = MyVaadinUI.class, widgetset = "com.analemma.vaadin_production_mode.AppWidgetSet")
+    public static class Servlet extends VaadinServlet {
+    }
+
+    @Override
+    public void init(VaadinRequest request) {
+    }
+}
