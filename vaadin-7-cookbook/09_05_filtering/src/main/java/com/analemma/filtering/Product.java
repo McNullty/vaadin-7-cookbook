44091fc86b2d8176f86be2c652b13d792f2491f6
Mladen Čikara <mladen.cikara@gmail.com>
2014-03-17 22:10:26 +0100

    Dodao poglavlje 09_05 Filtering. Dodavanje filtera na tablicu

---
+++ vaadin-7-cookbook/09_05_filtering/src/main/java/com/analemma/filtering/Product.java
@@ -0,0 +1,39 @@
+package com.analemma.filtering;
+
+public class Product {
+
+  private int id;
+  private String name;
+  private double price;
+
+  public Product(final int id, final String name, final double price) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(final int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(final String name) {
+    this.name = name;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public void setPrice(final double price) {
+    this.price = price;
+  }
+
+}
