4724bccd1aad1afedc86e143109d6343a830846a
Mladen Čikara <mladen.cikara@gmail.com>
2014-03-23 23:17:58 +0100

    Dodao serverside push na poglavlje 05_08

--- vaadin-7-cookbook/05_08_message_menu_bar/src/main/java/com/analemma/message_menu_bar/MessageMenuBar.java
+++ vaadin-7-cookbook/05_08_message_menu_bar/src/main/java/com/analemma/message_menu_bar/MessageMenuBar.java
@@ -1,7 +1,5 @@
 package com.analemma.message_menu_bar;
 
-import org.vaadin.artur.icepush.ICEPush;
-
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.MenuBar;
 import com.vaadin.ui.MenuBar.Command;
@@ -11,82 +9,82 @@ import com.vaadin.ui.Notification;
 @SuppressWarnings("serial")
 public class MessageMenuBar extends HorizontalLayout {
 
-	private MenuBar menubar = new MenuBar();
-	private MenuBar messageBar = new MenuBar();
-	private ICEPush pusher = new ICEPush();
-	private MenuBar.MenuItem messageMenu;	
-	
-	private Command menuCommand = new Command() {
-		public void menuSelected(MenuItem selectedItem) {
-			Notification.show("Action " + selectedItem.getText());
-		}
-	};
+  private final MenuBar menubar = new MenuBar();
+  private final MenuBar messageBar = new MenuBar();
+  private MenuBar.MenuItem messageMenu;
 
-	public MessageMenuBar() {
-		setWidth("100%");
-		menubar.setSizeFull();
-		fillMenu();
+  private final Command menuCommand = new Command() {
+    @Override
+    public void menuSelected(final MenuItem selectedItem) {
+      Notification.show("Action " + selectedItem.getText());
+    }
+  };
 
-		addComponent(menubar);
-		addComponent(messageBar);
-		addExtension(pusher);
+  public MessageMenuBar() {
+    setWidth("100%");
+    menubar.setSizeFull();
+    fillMenu();
 
-		new MessageCounter().start();
-	}
+    addComponent(menubar);
+    addComponent(messageBar);
 
-	private void fillMenu() {
-		final MenuBar.MenuItem fileItem = menubar.addItem("File", null);
-		final MenuBar.MenuItem newItem = fileItem.addItem("New", null);
+    new MessageCounter().start();
+  }
 
-		fileItem.addItem("Open file...", menuCommand);
-		fileItem.addSeparator();
+  private void fillMenu() {
+    final MenuBar.MenuItem fileItem = menubar.addItem("File", null);
+    final MenuBar.MenuItem newItem = fileItem.addItem("New", null);
 
-		newItem.addItem("File", menuCommand);
-		newItem.addItem("Folder", menuCommand);
-		newItem.addItem("Project...", menuCommand);
+    fileItem.addItem("Open file...", menuCommand);
+    fileItem.addSeparator();
 
-		fileItem.addItem("Save", menuCommand);
+    newItem.addItem("File", menuCommand);
+    newItem.addItem("Folder", menuCommand);
+    newItem.addItem("Project...", menuCommand);
 
-		final MenuBar.MenuItem edit = menubar.addItem("Edit", null);
-		edit.addItem("Cut", menuCommand);
-		edit.addItem("Copy", menuCommand);
-		edit.addItem("Paste", menuCommand);
+    fileItem.addItem("Save", menuCommand);
 
-		messageMenu = messageBar.addItem("New message: 0", null);
-	}
+    final MenuBar.MenuItem edit = menubar.addItem("Edit", null);
+    edit.addItem("Cut", menuCommand);
+    edit.addItem("Copy", menuCommand);
+    edit.addItem("Paste", menuCommand);
 
-	public class MessageCounter extends Thread {
-		
-		private int count;
-		private long nextTime;
-		private static final int TIME_INTERVAL = 5000;
+    messageMenu = messageBar.addItem("New message: 0", null);
+  }
 
-		@Override
-		public void run() {			
-			int oldCount = count;
-			while (true) {		
-				updateMessageCount();
-				if (oldCount != count) {
-					getUI().getSession().getLockInstance().lock();
-					try {
-						oldCount = count;
-						messageMenu.setText("New message:" + count);
-						pusher.push();							
-					} finally {
-						getUI().getSession().getLockInstance().unlock();
-					}
-				}
-			}
-		}
-		
-		private void updateMessageCount() {
-			long currentTime = System.currentTimeMillis();
-			if (currentTime > nextTime ) {
-				count++;
-				nextTime = currentTime + TIME_INTERVAL;
-			}
-		}
+  public class MessageCounter extends Thread {
 
-	}
+    private int count;
+    private long nextTime;
+    private static final int TIME_INTERVAL = 5000;
+
+    @Override
+    public void run() {
+      int oldCount = count;
+      while (true) {
+        updateMessageCount();
+        if (oldCount != count) {
+          oldCount = count;
+          getUI().access(new Runnable() {
+
+            @Override
+            public void run() {
+              messageMenu.setText("New message:" + count);
+              getUI().push();
+            }
+          });
+        }
+      }
+    }
+
+    private void updateMessageCount() {
+      final long currentTime = System.currentTimeMillis();
+      if (currentTime > nextTime) {
+        count++;
+        nextTime = currentTime + TIME_INTERVAL;
+      }
+    }
+
+  }
 
 }
--- vaadin-7-cookbook/05_08_message_menu_bar/src/main/java/com/analemma/message_menu_bar/MyVaadinUI.java
+++ vaadin-7-cookbook/05_08_message_menu_bar/src/main/java/com/analemma/message_menu_bar/MyVaadinUI.java
@@ -2,24 +2,24 @@ package com.analemma.message_menu_bar;
 
 import javax.servlet.annotation.WebServlet;
 
-import org.vaadin.artur.icepush.ICEPushServlet;
-
 import com.vaadin.annotations.Push;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.shared.communication.PushMode;
 import com.vaadin.ui.UI;
 
 // TODO: Osposobiti ovaj projekt i napraviti pravi server side push
 @Theme("mytheme")
 @SuppressWarnings("serial")
-@Push
+@Push(value = PushMode.MANUAL)
 public class MyVaadinUI extends UI {
 
   @WebServlet(value = "/*", asyncSupported = true)
   @VaadinServletConfiguration(productionMode = false, ui = MyVaadinUI.class,
       widgetset = "com.analemma.message_menu_bar.AppWidgetSet")
-  public static class Servlet extends ICEPushServlet {
+  public static class Servlet extends VaadinServlet {
   }
 
   @Override
