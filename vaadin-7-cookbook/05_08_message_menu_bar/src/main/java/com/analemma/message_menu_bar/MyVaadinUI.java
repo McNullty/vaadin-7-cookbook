1533fb10555032910c80fb35f16ec883091ec152
Mladen Čikara <mladen.cikara@gmail.com>
2014-03-23 23:03:40 +0100

    dodavanje Server side pusha u 05_08

--- vaadin-7-cookbook/05_08_message_menu_bar/src/main/java/com/analemma/message_menu_bar/MyVaadinUI.java
+++ vaadin-7-cookbook/05_08_message_menu_bar/src/main/java/com/analemma/message_menu_bar/MyVaadinUI.java
@@ -4,25 +4,28 @@ import javax.servlet.annotation.WebServlet;
 
 import org.vaadin.artur.icepush.ICEPushServlet;
 
+import com.vaadin.annotations.Push;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.UI;
 
-//TODO: Osposobiti ovaj projekt i napraviti pravi server side push
+// TODO: Osposobiti ovaj projekt i napraviti pravi server side push
 @Theme("mytheme")
 @SuppressWarnings("serial")
+@Push
 public class MyVaadinUI extends UI {
 
-	@WebServlet(value = "/*", asyncSupported = true)
-	@VaadinServletConfiguration(productionMode = false, ui = MyVaadinUI.class, widgetset = "com.analemma.message_menu_bar.AppWidgetSet")
-	public static class Servlet extends ICEPushServlet {
-	}
+  @WebServlet(value = "/*", asyncSupported = true)
+  @VaadinServletConfiguration(productionMode = false, ui = MyVaadinUI.class,
+      widgetset = "com.analemma.message_menu_bar.AppWidgetSet")
+  public static class Servlet extends ICEPushServlet {
+  }
 
-	@Override
-	protected void init(VaadinRequest request) {
+  @Override
+  protected void init(final VaadinRequest request) {
 
-		setContent(new MessageMenuBar());
-	}
+    setContent(new MessageMenuBar());
+  }
 
 }
